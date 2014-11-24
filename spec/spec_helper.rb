7528272d81af0a6115602b8ec2791a286ff84bb8
Tom Milewski <tmilewski@gmail.com>
2014-11-24 11:37:41 -0500

    Move RSpec configuration to .rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,6 @@ require 'rack'
 require 'p3p'
 
 RSpec.configure do |config|
-  config.color = true
-  config.formatter = :documentation
-  config.order = :random
-
   config.after(:each) do
     P3P.configuration.set_default_header!
   end
